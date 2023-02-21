925488874a411e4c723c03cfd8e1394a90b3e586
Семен Киреков <sakireko@mts.ru>
2023-02-21 20:41:33 +0300

    add static method Role.roots()

--- src/main/java/com/example/demo/domain/Role.java
+++ src/main/java/com/example/demo/domain/Role.java
@@ -1,5 +1,11 @@
 package com.example.demo.domain;
 
+import java.util.Set;
+
 public interface Role {
     boolean includes(Role role);
+
+    static Set<Role> roots() {
+        return Set.of(CommunityRoleType.ADMIN);
+    }
 }
