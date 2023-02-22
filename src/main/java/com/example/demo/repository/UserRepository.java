2832d89a463a5f55039e494088aa378c1314b3dc
Семен Киреков <sakireko@mts.ru>
2023-02-22 22:51:31 +0300

    add UserRepository

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
+}
