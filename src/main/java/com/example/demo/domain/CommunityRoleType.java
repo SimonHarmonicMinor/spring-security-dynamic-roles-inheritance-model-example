777fc39f933ed78958ec54621f9d1df27c273f82
Семен Киреков <sakireko@mts.ru>
2023-02-23 14:53:08 +0300

    add CommunityRoleType.SpringComponent

--- src/main/java/com/example/demo/domain/CommunityRoleType.java
+++ src/main/java/com/example/demo/domain/CommunityRoleType.java
@@ -1,8 +1,12 @@
 package com.example.demo.domain;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import lombok.Getter;
 
 public enum CommunityRoleType implements Role {
     ADMIN, MODERATOR;
@@ -17,5 +21,12 @@ public enum CommunityRoleType implements Role {
     @Override
     public boolean includes(Role role) {
         return this.equals(role) || children.stream().anyMatch(r -> r.includes(role));
+    }
+
+    @Component("CommunityRole")
+    @Getter
+    static class SpringComponent {
+        private final CommunityRoleType ADMIN = CommunityRoleType.ADMIN;
+        private final CommunityRoleType MODERATOR = CommunityRoleType.MODERATOR;
     }
 }
