98d0252d68db8c28a55c3e88f1d40226d144016d
Семен Киреков <sakireko@mts.ru>
2023-02-23 15:45:45 +0300

    add DatabaseUtil

---
+++ src/test/java/com/example/demo/controller/DatabaseUtil.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import java.util.List;
+
+public class DatabaseUtil {
+    private static final List<String> TABLES = List.of(
+        "community_role",
+        "post_role",
+        "post",
+        "community",
+        "users"
+    );
+
+    public static void cleanDatabase(JdbcTemplate jdbcTemplate) {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, TABLES.toArray(String[]::new));
+    }
+}
