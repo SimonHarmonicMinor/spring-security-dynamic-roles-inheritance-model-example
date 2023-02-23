503892a2996e22c84693a33858e7ba03e10bcfea
Семен Киреков <sakireko@mts.ru>
2023-02-23 14:58:13 +0300

    add PostRoleRepository

---
+++ src/main/java/com/example/demo/repository/PostRoleRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.PostRole;
+import com.example.demo.domain.PostRoleType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
+
+public interface PostRoleRepository extends JpaRepository<PostRole, Long> {
+    @Query("""
+        SELECT pr.type FROM PostRole pr
+        JOIN pr.post p
+        JOIN p.community c
+        WHERE pr.user.id = :userId AND c.id = :communityId
+        """)
+    Set<PostRoleType> findRoleTypesByUserIdAndCommunityId(Long userId, Long communityId);
+}
