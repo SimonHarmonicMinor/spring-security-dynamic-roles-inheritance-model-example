613ac820b10791643e7c2f0f2c9063e89b2cc9fc
Семен Киреков <sakireko@mts.ru>
2023-02-23 14:58:21 +0300

    add CommunityRoleRepository

---
+++ src/main/java/com/example/demo/repository/CommunityRoleRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.CommunityRole;
+import com.example.demo.domain.CommunityRoleType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
+
+public interface CommunityRoleRepository extends JpaRepository<CommunityRole, Long> {
+    @Query("""
+        SELECT cr.type FROM CommunityRole cr
+        WHERE cr.user.id = :userId AND cr.community.id = :communityId
+        """)
+    Set<CommunityRoleType> findRoleTypesByUserIdAndCommunityId(Long userId, Long communityId);
+}
