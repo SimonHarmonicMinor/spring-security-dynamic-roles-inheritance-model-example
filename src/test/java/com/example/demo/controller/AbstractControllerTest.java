c55d905f9478e5248db5f1938b430ec167dda92b
Semyon Kirekov <semyon@kirekov.com>
2023-03-11 15:23:40 +0300

    add testcontainers network to AbstractControllerTest

--- src/test/java/com/example/demo/controller/AbstractControllerTest.java
+++ src/test/java/com/example/demo/controller/AbstractControllerTest.java
@@ -1,24 +1,30 @@
 package com.example.demo.controller;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Testcontainers
 class AbstractControllerTest {
-    @Container
-    public static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:13.5");
 
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-    }
+  public static final Network NETWORK = Network.newNetwork();
+
+  @Container
+  public static final PostgreSQLContainer<?> POSTGRES =
+      new PostgreSQLContainer<>("postgres:13.5")
+          .withNetwork(NETWORK);
+
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+    registry.add("spring.datasource.username", POSTGRES::getUsername);
+    registry.add("spring.datasource.password", POSTGRES::getPassword);
+  }
 }
