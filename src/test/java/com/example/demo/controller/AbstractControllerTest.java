beaf1f0674bf2f5197751c7856d1773b495a6747
Semyon Kirekov <semyon@kirekov.com>
2023-03-11 22:05:25 +0300

    AbstractControllerTest usess ApplicationContextInitializer

--- src/test/java/com/example/demo/controller/AbstractControllerTest.java
+++ src/test/java/com/example/demo/controller/AbstractControllerTest.java
@@ -2,30 +2,45 @@ package com.example.demo.controller;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.example.demo.controller.AbstractControllerTest.Initializer;
+import java.util.Map;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.lifecycle.Startables;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@Testcontainers
+@ContextConfiguration(initializers = Initializer.class)
 class AbstractControllerTest {
 
   public static final Network NETWORK = Network.newNetwork();
 
-  @Container
   public static final PostgreSQLContainer<?> POSTGRES =
       new PostgreSQLContainer<>("postgres:13.5")
           .withNetworkAliases("postgres")
           .withNetwork(NETWORK);
 
-  @DynamicPropertySource
-  static void setProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-    registry.add("spring.datasource.username", POSTGRES::getUsername);
-    registry.add("spring.datasource.password", POSTGRES::getPassword);
+  static class Initializer implements
+      ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext context) {
+      Startables.deepStart(POSTGRES).join();
+
+      context.getEnvironment()
+          .getPropertySources()
+          .addFirst(new MapPropertySource(
+              "testcontainers",
+              Map.of(
+                  "spring.datasource.url", POSTGRES.getJdbcUrl(),
+                  "spring.datasource.username", POSTGRES.getUsername(),
+                  "spring.datasource.password", POSTGRES.getPassword()
+              )
+          ));
+    }
   }
 }
